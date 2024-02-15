cd76fb95340046c99de918c818d6f9b59599d320
Thiago <thiagosmorato@gmail.com>
2024-02-15 16:15:33 -0300

    Fórum com DDD & Clean Architecture: Caso de uso - Comentar na resposta

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,33 @@
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { makeAnswer } from "test/factories/make-answer";
+import { CommentOnAnswerUseCase } from "./comment-on-answer";
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository";
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+/**
+ * sut = System Under Test
+ */
+let sut: CommentOnAnswerUseCase;
+
+describe("Comment on Answer", () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
+    sut = new CommentOnAnswerUseCase(inMemoryAnswersRepository, inMemoryAnswerCommentsRepository);
+  });
+
+  it("should be able to comment on answer", async () => {
+    const answer = makeAnswer();
+
+    await inMemoryAnswersRepository.create(answer);
+
+    await sut.execute({
+      authorId: answer.authorId.toString(),
+      answerId: answer.id.toString(),
+      content: "Comentário teste",
+    });
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual("Comentário teste");
+  });
+});
